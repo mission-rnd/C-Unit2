d3a8432108a0a8ba3e7e4db2117ee060b7e7ee2c
chinna samudrudu <c.samudrudu333@gmail.com>
2018-12-14 22:21:42 +0530

    fix errors in e04_memory spec

--- spec/E04_MemorySpec.cpp
+++ spec/E04_MemorySpec.cpp
@@ -59,54 +59,54 @@ namespace spec
 		void TestprimeFactorsSmall(){
 			DynamicList *primes;
 			primes = primeFactors(10);
-			int ans[] = { 2, 5 };
+			int ans1[] = { 2, 5 };
 			Assert::AreEqual(2, primes->count, L"TestprimeFactorsSmall() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 2), L"TestprimeFactorsSmall() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans1, 2), L"TestprimeFactorsSmall() failed", 1, 2);
 			primes = primeFactors(75);
-			int ans[] = { 3, 5, 5 };
+			int ans2[] = { 3, 5, 5 };
 			Assert::AreEqual(3, primes->count, L"TestprimeFactorsSmall() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 3), L"TestprimeFactorsSmall() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans2, 3), L"TestprimeFactorsSmall() failed", 1, 2);
 			
 			primes = primeFactors(96);
-			int ans[] = { 2, 2, 2, 2, 2, 3 };
+			int ans3[] = { 2, 2, 2, 2, 2, 3 };
 			Assert::AreEqual(6, primes->count, L"TestprimeFactorsSmall() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 6), L"TestprimeFactorsSmall() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans3, 6), L"TestprimeFactorsSmall() failed", 1, 2);
 		}
 
 		[TestMethod, Timeout(3000)]
 		void TestprimeFactorsMedium(){
 			DynamicList *primes;
 			primes = primeFactors(19250);
-			int ans[] = { 2, 5, 5, 5, 7, 11 };
+			int ans1[] = { 2, 5, 5, 5, 7, 11 };
 			Assert::AreEqual(6, primes->count, L"TestprimeFactorsMedium() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 6), L"TestprimeFactorsMedium() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans1, 6), L"TestprimeFactorsMedium() failed", 1, 2);
 			primes = primeFactors(31247);
-			int ans[] = { 31247 };
+			int ans2[] = { 31247 };
 			Assert::AreEqual(1, primes->count, L"TestprimeFactorsMedium() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 1), L"TestprimeFactorsMedium() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans2, 1), L"TestprimeFactorsMedium() failed", 1, 2);
 
 			primes = primeFactors(78057);
-			int ans[] = { 3, 3, 3, 7, 7, 59 };
+			int ans3[] = { 3, 3, 3, 7, 7, 59 };
 			Assert::AreEqual(6, primes->count, L"TestprimeFactorsMedium() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 6), L"TestprimeFactorsMedium() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans3, 6), L"TestprimeFactorsMedium() failed", 1, 2);
 		}
 
 		[TestMethod, Timeout(3000)]
 		void TestprimeFactorsLarge(){
 			DynamicList *primes;
 			primes = primeFactors(1073741824);
-			int ans[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+			int ans1[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
 			Assert::AreEqual(30, primes->count, L"TestprimeFactorsLarge() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 30), L"TestprimeFactorsLarge() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans1, 30), L"TestprimeFactorsLarge() failed", 1, 2);
 			primes = primeFactors(26165843);
-			int ans[] = { 11, 71, 33503 };
+			int ans2[] = { 11, 71, 33503 };
 			Assert::AreEqual(3, primes->count, L"TestprimeFactorsLarge() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 3), L"TestprimeFactorsLarge() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans2, 3), L"TestprimeFactorsLarge() failed", 1, 2);
 
 			primes = primeFactors(691979840);
-			int ans[] = { 2, 2, 2, 2, 2, 2, 5, 23, 149, 631 };
+			int ans3[] = { 2, 2, 2, 2, 2, 2, 5, 23, 149, 631 };
 			Assert::AreEqual(10, primes->count, L"TestprimeFactorsLarge() failed", 1, 2);
-			Assert::AreEqual(true, areEqualArrays(primes->list, ans, 10), L"TestprimeFactorsLarge() failed", 1, 2);
+			Assert::AreEqual(true, areEqualArrays(primes->list, ans3, 10), L"TestprimeFactorsLarge() failed", 1, 2);
 		}
     };
 }
